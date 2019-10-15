b7bb37ad92f6ebdd87cb67839f3cc6451ace143e
xiaochen98756 <1047800119@qq.com>
2019-10-15 22:55:35 +0800

    TDD 打卡点5 只考虑Schme的输入与解析

--- src/main/java/TDD/Schmes.java
+++ src/main/java/TDD/Schmes.java
@@ -1,8 +1,11 @@
 package TDD;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Schmes {
@@ -19,12 +22,28 @@ public class Schmes {
 
     public Object getValue(String name,String strVal){
          String type=schmes.get(name);
-         switch (type){
+         switch (type) {
              case "bool":
                  return "true".equalsIgnoreCase(strVal);
              case "int":
                  return Integer.parseInt(strVal);
-
+             case "str":
+                 return String.valueOf(strVal);
+             case "strlist":
+                 List<String> asList;
+                 try {
+                     asList = Arrays.asList(strVal.split(","));
+                 } catch (Exception e) {
+                     return String.valueOf(Boolean.FALSE);
+                 }
+                 return Arrays.toString(asList.toArray());
+             case "map":
+                 JSONObject object = JSONObject.parseObject(strVal);
+                 Map<String ,Object> map=new HashMap<>();
+                 for (String key:object.keySet()) {
+                     map.put(key,object.get(key));
+                 }
+                 return map.toString();
              default:
                  return strVal;
          }
--- src/test/java/TDD/SchmeTest.java
+++ src/test/java/TDD/SchmeTest.java
@@ -3,6 +3,8 @@ package TDD;
 import TDD.Schmes;
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class SchmeTest {
@@ -22,6 +24,20 @@ public class SchmeTest {
 
     }
 
+    @Test
+    public void test_multi(){
+        Schmes schmes=new Schmes("l:str,t:str,w:strlist,k:strlist,o:map");
+        assertEquals(schmes.getValue("l","hello"),"hello");
+        assertEquals(schmes.getValue("t","world"),"world");
+        assertEquals(schmes.getValue("w","qqqq,wwww,aaaa,eeee"),"[qqqq, wwww, aaaa, eeee]");
+        assertEquals(schmes.getValue("k",null),String.valueOf(Boolean.FALSE));
+        assertEquals(schmes.getValue("k",""),String.valueOf("[]"));
+        assertEquals(schmes.getValue("o","{\"boolean\":true,\"string\":\"string\",\"list\":[1,2,3],\"int\":2}"),
+                "{boolean=true, string=string, list=[1,2,3], int=2}");
+        assertEquals(schmes.getValue("o","{}"),
+                "{}");
+
+    }
     @Test
     public void test_String(){
         Schmes schmes=new Schmes("l:str");
